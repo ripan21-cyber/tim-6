2d2be77865608ed016e377e371867b268d7f2508
Faiza <faiza@gmail.com>
2026-04-14 11:45:34 +0700

    p

--- header/menu.h
+++ header/menu.h
@@ -1,7 +1,29 @@
-#ifndef MENU_H
-#define MENU_H
+#include <stdio.h>
+#include "header/alat.h"
 
-void menuAdmin();
-void menuUser(char username[]);
+void menuAdmin() {
+    int pilihan;
 
-#endif
+    do {
+        printf("\n=== MENU ADMIN ===");
+        printf("\n1. Tambah Alat");
+        printf("\n2. Tampilkan Alat");
+        printf("\n3. Edit Alat");
+        printf("\n4. Hapus Alat");
+        printf("\n0. Logout");
+        printf("\nPilih: ");
+        scanf("%d", &pilihan);
+
+        switch(pilihan) {
+            case 1: tambahAlat(); break;
+            case 2: tampilAlat(); break;
+            case 3: editAlat(); break;
+            case 4: hapusAlat(); break;
+        }
+    } while(pilihan != 0);
+}
+
+int main() {
+    menuAdmin();
+    return 0;
+}
